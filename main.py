260fdc7bcb6ec917bdd5dd3d6eaa85d281fd2989
addddd123 <adshe886@gmail.com>
2019-02-25 00:42:34 +0530

    added edit function

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request,session
+
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import json
@@ -43,6 +44,7 @@ class Posts(db.Model):
     tagline = db.Column(db.String(120), nullable=False)
     date = db.Column(db.String(12), nullable=True)
     img_file = db.Column(db.String(12), nullable=True)
+   
 
 @app.route("/")
 def home():
@@ -69,11 +71,29 @@ def dashbord():
            
             return render_template('dashbord.html', params=params,posts=posts)
     
-        
+
+    
     return render_template('login.html', params=params)
+    
+@app.route("/edit/<string:sno>",methods=['GET','POST'])
+def edit(sno):
+    if ('user' in session and session['user']==params['user']):
+        
+        if request.method=="POST":
+           
+            box_title=request.form.get('title')
+            tline=request.form.get('tline')
+            slug=request.form.get('slug')
+            content=request.form.get('content')
+            img_file=request.form.get('img_file')
+            date=datetime.now()
+            if sno=='0':
+                post=Posts(title=box_title,slug=slug,content=content,tagline=tline,date=date,img_file=img_file)
+                db.session.add(post)
+                db.session.commit()
+        return render_template('edit.html',params=params,sno=sno)
 
-
-
+    
 @app.route("/about")
 def about():
     return render_template('about.html', params=params)
@@ -96,8 +116,8 @@ def contact():
         phone = request.form.get('phone')
         message = request.form.get('message')
         entry = Contacts(name=name, phone_num = phone, mes = message, date= datetime.now(),email = email )
-        db.Sesssion.add(entry)
-        db.Sesssion.commit()
+        db.session.add(entry)
+        db.session.commit()
         mail.send_message('New message from ' + name,
                         sender=email,
                         recipients = [params['gmail-user']],
