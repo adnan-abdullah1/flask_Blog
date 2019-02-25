662772cefca7c1dc519af8161808f8c432bea698
addddd123 <adshe886@gmail.com>
2019-02-26 00:27:02 +0530

    added_pagination

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import json
 from flask_mail import Mail
-
+import math
 
 with open('config.json', 'r') as c:
     params = json.load(c)["params"]
@@ -48,10 +48,28 @@ class Posts(db.Model):
 
 @app.route("/")
 def home():
-    posts = Posts.query.filter_by().all()[0:params['no_of_posts']]
-    return render_template('index.html', params=params, posts=posts)
-
-
+    posts = Posts.query.filter_by().all()
+    last=math.floor(len(posts)/int(params['no_of_posts']))
+    page=request.args.get('page')
+    if(not str(page).isnumeric()):
+        page=1
+    page=int(page)
+    posts=posts[(page-1)*int(params['no_of_posts']):(page-1)*int(params['no_of_posts'])+int(params['no_of_posts'])]
+    if page==1:
+        prev="#"
+        next="/?page="+str(page+1)
+    elif page==last:
+        prev="/?page="+str(page-1)
+        next="#"
+    else:
+        prev="/?page"+str(page-1)
+        next="/?page"+str(page+1)
+    return render_template('index.html',params=params,posts=posts,prev=prev,next=next)
+    
+    
+    
+    
+    
 
 @app.route("/dashbord",methods=['GET','POST'])
 def dashbord():
