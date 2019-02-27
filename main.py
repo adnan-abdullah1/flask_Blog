9600ea290da8071f3a2cc0210c624795e9a03561
addddd123 <adshe886@gmail.com>
2019-02-27 17:33:18 +0530

    correct pagination

--- main.py
+++ main.py
@@ -49,12 +49,15 @@ class Posts(db.Model):
 @app.route("/")
 def home():
     posts = Posts.query.filter_by().all()
-    last=math.ceil(len(posts)/int(params['no_of_posts']))
+    print(len(posts))
+    last=math.ceil((len(posts)/int(params['no_of_posts'])))
     page=request.args.get('page')
     if(not str(page).isnumeric()):
         page=1
+       
     page=int(page)
     posts=posts[(page-1)*int(params['no_of_posts']):(page-1)*int(params['no_of_posts'])+int(params['no_of_posts'])]
+    print(posts)
     if page==1:
         prev="#"
         next="/?page="+str(page+1)
@@ -62,8 +65,8 @@ def home():
         prev="/?page="+str(page-1)
         next="#"
     else:
-        prev="/?page"+str(page-1)
-        next="/?page"+str(page+1)
+        prev="/?page="+str(page-1)
+        next="/?page="+str(page+1)
     return render_template('index.html',params=params,posts=posts,prev=prev,next=next)
     
     #posts = Posts.query.filter_by().all()[0:params['no_of_posts']]
